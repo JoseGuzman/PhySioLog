f1f9379eb9b4427fd533b4b81abbe86c6b090b16
JoseGuzman <sjm.guzman@gmail.com>
2026-02-16 09:15:00 +0100

    update WSGI usage instructions for local development

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,8 @@ Author: Jose Guzman, sjm.guzman<at>gmail.com
 WSGI entrypoint for production servers (Gunicorn).
 
 Usage:
->>> gunicorn wsgi:app -w 4 -b 0.0.0:8000
+To run the Flask locally
+>>> uv run gunicorn -w 2 -b 0.0.0.0:8000 wsgi:app --access-logfile - --error-logfile - --timeout 60
 """
 
 from physiolog import create_app
